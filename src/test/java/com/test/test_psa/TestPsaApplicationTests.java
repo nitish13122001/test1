c93f6ca3a98e075a72300f0f0790f57d5dbfad01
nitish13122001 <mail2nitish.sharma@gmail.com>
2025-07-31 10:26:42 +0530

    project to check conflicts

---
+++ src/test/java/com/test/test_psa/TestPsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.test_psa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
